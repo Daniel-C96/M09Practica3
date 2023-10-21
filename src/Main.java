178ed26501aae694b3c8888ec9e956e63e5e8ff5
Daniel C <Danielcontaf1996@gmail.com>
2023-10-21 04:25:18 +0200

    Código mejorado con funciones

--- src/Main.java
+++ src/Main.java
@@ -7,110 +7,112 @@ import java.util.Scanner;
 
 public class Main {
     static final Scanner input = new Scanner(System.in);
+    static final int KEY_LENGTH = 192; // Tamaño de la clave en bits
 
     public static void main(String[] args) {
-
-        //Especifica la ruta del archivo que deseas abrir
         System.out.println("Escribe la ruta del fichero:");
         String rutaArchivo = input.nextLine();
 
-        //Loop para que pregunte hasta que se introduzca un fichero que existe
+        FileInputStream archivo = buscarArchivoExistente(rutaArchivo);
+
+        if (archivo != null) {
+            try {
+                String password = obtenerPassword();
+                byte[] key = generarClaveDesdePassword(password);
+                SecretKey clave = new SecretKeySpec(key, "AES");
+
+                byte[] bytesOriginal = leerArchivo(archivo);
+                System.out.println("Contenido del archivo es: " + new String(bytesOriginal, "UTF-8"));
+
+                Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+
+                if (!rutaArchivo.endsWith(".aes")) {
+                    System.out.println("Entrando en modo cifrado...");
+                    cifrarYGuardarArchivo(cipher, clave, rutaArchivo, bytesOriginal);
+                    sobreescribirYBorrarArchivo(bytesOriginal, rutaArchivo);
+                } else {
+                    System.out.println("Entrando en modo descifrado...");
+                    try {
+                        descifrarYGuardarArchivo(cipher, clave, rutaArchivo, bytesOriginal);
+                        sobreescribirYBorrarArchivo(bytesOriginal, rutaArchivo);
+                    } catch (Exception e) {
+                        System.out.print("La contraseña que has introducido no es correcta...");
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static FileInputStream buscarArchivoExistente(String rutaArchivo) {
         FileInputStream archivo = null;
         while (archivo == null) {
             try {
                 archivo = new FileInputStream(rutaArchivo);
-            } catch (Exception FileNotFoundException) {
+            } catch (FileNotFoundException ex) {
                 System.out.println("Archivo no encontrado... Escribe uno valido");
                 rutaArchivo = input.nextLine();
-
             }
         }
-        try {
-            //Contraseña y clave simetrica por teclado
-            System.out.println("Introduce la contraseña:");
-            String password = input.nextLine();
-            byte[] data = password.getBytes("UTF-8");
-            MessageDigest md = MessageDigest.getInstance("SHA-256");
-            byte[] hash = md.digest(data);
-            byte[] key = Arrays.copyOf(hash, 192 / 8);
-            SecretKey clau = new SecretKeySpec(key, "AES");
-
-
-            // Crea un array de bytes del tamaño del archivo y almacena ahi el archivo leyendolo
-            byte[] bytesOriginal = new byte[archivo.available()];
-            archivo.read(bytesOriginal);
-
-            String contenido = new String(bytesOriginal, "UTF-8");
-            System.out.println("Contenido del archivo es: " + contenido);
-
-            //Generar Cipher
-            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-
-            if (!rutaArchivo.endsWith(".aes")) {
-                System.out.println("Entrando en modo cifrado...");
-                cifrar(cipher, clau, rutaArchivo, bytesOriginal);
-                sobreescribirBorrar(bytesOriginal, rutaArchivo);
-            } else {
-                System.out.println("Entrando en modo descifrado...");
-                try {
-                    descifrar(cipher, clau, rutaArchivo, bytesOriginal);
-                    sobreescribirBorrar(bytesOriginal, rutaArchivo);
-                } catch (Exception e) {
-                    System.out.print("La contraseña que has introducido no es correcta...");
-                }
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        return archivo;
     }
 
-    public static void cifrar(Cipher cipher, SecretKey clau, String rutaArchivo, byte[] bytesOriginal) throws Exception {
-        cipher.init(Cipher.ENCRYPT_MODE, clau);
+    public static String obtenerPassword() {
+        System.out.println("Introduce la contraseña:");
+        return input.nextLine();
+    }
 
-        // Cifrar el archivo
+    public static byte[] generarClaveDesdePassword(String password) throws Exception {
+        byte[] data = password.getBytes("UTF-8");
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        byte[] hash = md.digest(data);
+        return Arrays.copyOf(hash, KEY_LENGTH / 8);
+    }
+
+    public static byte[] leerArchivo(FileInputStream archivo) throws IOException {
+        byte[] bytesOriginal = new byte[archivo.available()];
+        archivo.read(bytesOriginal);
+        return bytesOriginal;
+    }
+
+    public static void cifrarYGuardarArchivo(Cipher cipher, SecretKey clave, String rutaArchivo, byte[] bytesOriginal) throws Exception {
+        cipher.init(Cipher.ENCRYPT_MODE, clave);
         byte[] bytesCifrados = cipher.doFinal(bytesOriginal);
 
-        //Guardar el archivo cifrado
         String archivoCifrado = rutaArchivo + ".aes";
-        FileOutputStream archivoSalida = new FileOutputStream(archivoCifrado);
-        archivoSalida.write(bytesCifrados);
-        archivoSalida.close();
+        try (FileOutputStream archivoSalida = new FileOutputStream(archivoCifrado)) {
+            archivoSalida.write(bytesCifrados);
+        }
 
         System.out.println("Archivo cifrado con éxito en: " + archivoCifrado);
         System.out.println("El contenido cifrado es: " + new String(bytesCifrados, "UTF-8"));
-
     }
 
-    public static void descifrar(Cipher cipher, SecretKey clau, String rutaArchivo, byte[] bytesOriginal) throws Exception {
-        cipher.init(Cipher.DECRYPT_MODE, clau);
-
-        // Descifrar el archivo
+    public static void descifrarYGuardarArchivo(Cipher cipher, SecretKey clave, String rutaArchivo, byte[] bytesOriginal) throws Exception {
+        cipher.init(Cipher.DECRYPT_MODE, clave);
         byte[] bytesDescifrados = cipher.doFinal(bytesOriginal);
 
-        // Guardar el archivo descifrado
         String archivoDescifrado = rutaArchivo.substring(0, rutaArchivo.length() - 4);
-        FileOutputStream archivoSalida = new FileOutputStream(archivoDescifrado);
-        archivoSalida.write(bytesDescifrados);
-        archivoSalida.close();
+        try (FileOutputStream archivoSalida = new FileOutputStream(archivoDescifrado)) {
+            archivoSalida.write(bytesDescifrados);
+        }
 
         System.out.println("Archivo descifrado con éxito en: " + archivoDescifrado);
         System.out.println("El contenido descifrado es: " + new String(bytesDescifrados, "UTF-8"));
-
     }
 
-    public static void sobreescribirBorrar(byte[] bytesOriginal, String rutaArchivo) throws Exception {
-        // Sobreescribir con 0s el archivo original
+    public static void sobreescribirYBorrarArchivo(byte[] bytesOriginal, String rutaArchivo) throws IOException {
         Arrays.fill(bytesOriginal, (byte) 0);
-        FileOutputStream archivoOriginalSalida = new FileOutputStream(rutaArchivo);
-        archivoOriginalSalida.write(bytesOriginal);
-        archivoOriginalSalida.close();
+        try (FileOutputStream archivoOriginalSalida = new FileOutputStream(rutaArchivo)) {
+            archivoOriginalSalida.write(bytesOriginal);
+        }
 
-        //Eliminar archivo original
         File archivoOriginal = new File(rutaArchivo);
-        archivoOriginal.delete();
-
-        System.out.println("El archivo original se ha sobreescrito con 0s y borrado del sistema...");
-
+        if (archivoOriginal.delete()) {
+            System.out.println("El archivo original se ha sobreescrito con 0s y borrado del sistema...");
+        } else {
+            System.out.println("No se pudo eliminar el archivo original.");
+        }
     }
 }
